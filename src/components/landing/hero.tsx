257626da00e269d814f0ad004a8038cc9801cc25
Pratham Gambhir <prathamgambhir0307@gmail.com>
2026-01-09 19:19:47 +0530

    add icons

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -1,15 +1,19 @@
-import Container from "@/components/common/container";
 import { MapPin } from "lucide-react";
 import Image from "next/image";
 import ProfileImage from "../../../public/profile.jpeg";
 import { cn } from "@/lib/utils";
-import LiveClock from "../live-clock";
+import LiveClock from "./live-clock";
 import TypescriptIcon from "../Icons/typescript";
 import ReactIcon from "../Icons/react";
 import NextjsIcon from "../Icons/nextjs";
 import ExpressIcon from "../Icons/express";
 import MongoDBIcon from "../Icons/mongodb";
 import PostgresqlIcon from "../Icons/postgresql";
+import GithubIcon from "../Icons/github";
+import LinkedInIcon from "../Icons/linkedIn";
+import TwitterIcon from "../Icons/twitter-x";
+import MailIcon from "../Icons/email";
+import ResumeIcon from "../Icons/resume";
 
 export default function Hero() {
   return (
@@ -25,7 +29,7 @@ export default function Hero() {
             Pratham Gambhir
             <button
               className={cn(
-                "hidden md:flex text-[10px]/tight h-5 tracking-normal font-medium self-end md:px-1 items-center md:gap-1 button-inset"
+                "hidden md:flex text-[10px]/tight h-5 tracking-normal font-medium self-end md:px-2 items-center md:gap-1 button-inset"
               )}
             >
               {/* availability dot */}
@@ -45,7 +49,7 @@ export default function Hero() {
           {/* mobile open to work */}
           <button
             className={cn(
-              "flex md:hidden text-[10px]/tight h-5 tracking-normal font-medium self-start  mt-2 px-1 items-center gap-1 button-inset"
+              "flex md:hidden text-[10px]/tight h-5 tracking-tighter font-medium self-start  mt-2 px-1 items-center gap-1 button-inset"
             )}
           >
             {/* availability dot */}
@@ -53,7 +57,7 @@ export default function Hero() {
               <span className="availability-ping"></span>
               <span className="availability-pulse"></span>
             </span>
-            Open to work
+            <span className="tracking-tigher">Open to work</span>
           </button>
         </div>
         {/* pfp */}
@@ -73,21 +77,50 @@ export default function Hero() {
           Yeah! I'm a{" "}
           <span className="about-text-span">Full Stack Developer</span>.Sounds
           fancy, right? But here's the actual part-- I enjoy building things
-          that do something useful. From creating modern web applications using{" "}
-          <span className="about-skill-span"><ReactIcon className="size-4 rounded-3xl"/>React</span>,{" "}
-          <span className="about-skill-span"><NextjsIcon className="size-4 rounded-3xl dark:bg-white"/>NextJs</span>,{" "}
-          <span className="about-skill-span"><ExpressIcon className="size-4 rounded-3xl dark:bg-white"/>Express</span>&nbsp;and&nbsp;
+          that do something useful. From creating modern websites using{" "}
+          <span className="about-skill-span">
+            <ReactIcon className="size-4 rounded-3xl" />
+            React
+          </span>
+          ,{" "}
+          <span className="about-skill-span">
+            <NextjsIcon className="size-4 rounded-3xl dark:bg-white" />
+            NextJs
+          </span>
+          ,{" "}
+          <span className="about-skill-span">
+            <ExpressIcon className="size-4 rounded-3xl dark:bg-white" />
+            Express
+          </span>
+          &nbsp;and&nbsp;
           <span className="about-skill-span">
             <TypescriptIcon className="size-4 rounded-3xl" />
             Typescript
           </span>{" "}
-          backed by <span className="about-skill-span"><MongoDBIcon className="size-4 rounded-3xl"/>MongoDB</span>
+          backed by{" "}
+          <span className="about-skill-span">
+            <MongoDBIcon className="size-4 rounded-3xl" />
+            MongoDB
+          </span>
           &nbsp;and&nbsp;
-          <span className="about-skill-span"><PostgresqlIcon className="size-4 rounded-3xl"/>Postgresql</span> to shaping clean
-          APIs or polishing front-end interactions. I enjoy turning ideas into
-          working products and Yup! I breathe{" "}
+          <span className="about-skill-span">
+            <PostgresqlIcon className="size-4 rounded-3xl" />
+            Postgresql
+          </span>{" "}
+          to shaping clean APIs or polishing FrontEnd interactions.I enjoy
+          turning ideas into working products and Yup! I breathe{" "}
           <span className="about-text-span">code</span> most of the time.
         </p>
+      </div>
+      {/* Links */}
+      <div className="flex items-center gap-2 text-sm/relaxed w-full pt-6">
+        <div className="links-span flex items-center gap-2 py-2"><TwitterIcon className="size-3"/>DM</div>
+        <span>OR</span>
+        <div  className="links-span flex items-center gap-2 py-2"><MailIcon className="size-4"/>Email Me</div>
+        <span>|</span>
+        <div className="links-span flex items-center gap-1 py-2"><GithubIcon className="size-4"/></div>
+        <div className="links-span flex items-center gap-1 py-2"><LinkedInIcon className="size-4"/></div>
+        <div className="links-span flex items-center gap-1 py-2"><ResumeIcon className="size-4"/></div>
       </div>
     </div>
   );
